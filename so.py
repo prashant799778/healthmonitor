fea821fc631087920d22100f24d5623991a8d68e
sharad <sharad.fourbrick@gmail.com>
2019-12-16 15:34:59 +0530

    test

--- so.py
+++ so.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 import socketio
+import flaskext.couchdb
+from flask.ext.socketio import SocketIO
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
