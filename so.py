6fe2d473fa2653ba762dbae608d076df14f0ed26
sharad <sharad.fourbrick@gmail.com>
2019-12-16 15:15:59 +0530

    Update so.py

--- so.py
+++ so.py
@@ -8,18 +8,18 @@ socketio = SocketIO(app)
 
 
 
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
+# @socketio.on('message')
+# def handle_message(message):
+    # print('received message: ' + message)
 
 
 @socketio.on('new message')
 def handle_json(json):
     print('received json: ' + str(json))
     
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    print('received json: ' + str(json))    
+# @socketio.on('my event')
+# def handle_my_custom_event(json):
+    # print('received json: ' + str(json))    
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5054, debug=True) 
