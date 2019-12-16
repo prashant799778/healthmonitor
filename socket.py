019758f8224d12c3dca4a2db9bd3b129f5c767fa
sharad <sharad.fourbrick@gmail.com>
2019-12-16 14:36:29 +0530

    Create socket.py

---
+++ socket.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+
+
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))    
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5054, debug=True) 
+    socketio.run(app)
