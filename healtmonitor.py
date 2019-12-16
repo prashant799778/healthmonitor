de3fb3b4ee26ac6dc0412a1af0269651fee19e1e
prashant <prashantgoyal.fourbrick@gmail.com>
2019-12-16 12:47:55 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -612,11 +612,53 @@ def update_Patient_Vital_master():
 
 
 
+
+import socketio
+import eventlet
+import eventlet.wsgi
+from flask import Flask, render_template
+
+sio = socketio.Server()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Serve the client-side application."""
+    return render_template('index.html')
+
+@sio.on('connect', namespace='/')
+def connect(sid, environ):
+    print("connect ", sid)
+
+@sio.on('add user', namespace='/')
+def login1(sid, environ):
+    print("login1 ", sid)
+    sio.emit('login1', room=sid)
+
+@sio.on('new message', namespace='/')
+def message(sid, data):
+    print("message ", data)
+    sio.emit('reply', room=sid)
+
+@sio.on('disconnect', namespace='/')
+def disconnect(sid):
+    print('disconnect ', sid)
+
+# if __name__ == '__main__':
+#     # wrap Flask application with engineio's middleware
+#     app = socketio.Middleware(sio, app)
+
+#     # deploy as an eventlet WSGI server
+#     eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
+
 if __name__ == "__main__":
     CORS(app, support_credentials=True)
     app.run(host='0.0.0.0',port=5053,debug=True)
-    socketio.run(app,port=5053,debug=True)
+    #socketio.run(app,port=5053,debug=True)
+    app = socketio.Middleware(sio, app)
 
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 5053)), app)
    
 
 
