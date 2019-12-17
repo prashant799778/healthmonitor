d8cc9c2540000705d6103f9726ff35adeb821431
prashant <prashantgoyal.fourbrick@gmail.com>
2019-12-17 15:33:00 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -505,7 +505,7 @@ def update_Patient_type():
        
         json1=request.get_data() 
         data=json.loads(json1.decode("utf-8")) 
-        query2= "select * from Patient_master where Status<>'2' "
+        query2= "select * from Patient_master where Status<>'2'  and PatientId = '" + str(data["PatientId"])+ "' "
         conn=Connection()
         cursor = conn.cursor()
         cursor.execute(query2)
@@ -514,7 +514,7 @@ def update_Patient_type():
         conn.commit()
         cursor.close()
         print("yy")
-        query1 = " update Patient_master set  PatientName ='" + str(data["PatientName"]) + "' , DeviceMac ='" + str(data["DeviceMac"]) + "' , Bed_Number = '" + str(data["Bed_Number"]) + "', Usertype_Id = '" + str(data["Usertype_Id"]) + "' , hospital_Name ='" + str(data["hospital_Name"]) + "' , startdate='" + str(data["startdate"]) + "', enddate = '" + str(data["enddate"]) + "'  ,  UserUpdate ='" + str(data["UserUpdate"]) + "' , Status ='1'  where PatientId = '" + str(data["PatientId"])+ "';"
+        query1 = " update Patient_master set  PatientName ='" + str(data["PatientName"]) + "' , DeviceMac ='" + str(data["DeviceMac"]) + "' , Bed_Number = '" + str(data["Bed_Number"]) + "', Usertype_Id = '" + str(data["Usertype_Id"]) + "' , hospital_Name ='" + str(data["hospital_Name"]) + "' , startdate='" + str(data["startdate"]) + "', enddate = '" + str(data["enddate"]) + "'  ,  UserUpdate ='" + str(data["UserUpdate"]) + "' , Status='1'  where PatientId = '" + str(data["PatientId"])+ "';"
         print(query1)
         conn=Connection()
         cursor = conn.cursor()
