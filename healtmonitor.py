4963550c60e62dcf1dd900a10f4c73b85da26e10
prashant <prashantgoyal.fourbrick@gmail.com>
2019-12-17 12:48:58 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -586,19 +586,20 @@ def Patient_Vital_master_select():
         data = cursor.fetchall()
         cursor.close()
 
-        # for  data1 in data:
+        for  data1 in data:
             
-        #     data[data1]["ECG"]=json.loads(data1[data1]["ECG"].replace("'",'"'))
-        #     data[data1]["NIBP"]=json.loads(data1[data]["NIBP"].replace("'",'"'))
-        #     data[data1]["SPO2"]=json.loads(data1[data1]["SPO2"].replace("'",'"'))
-        #     print("prashant",data)
+            data[data1]["ECG"]=json.loads(data[data1]["ECG"].replace("'",'"'))
+            data[data1]["NIBP"]=json.loads(data[data]["NIBP"].replace("'",'"'))
+            data[data1]["SPO2"]=json.loads(data[data1]["SPO2"].replace("'",'"'))
+
+        print("prashant",data)
         
-        data[-1]["ECG"]=json.loads(data[-1]["ECG"].replace("'",'"'))
-        data[-1]["NIBP"]=json.loads(data[-1]["NIBP"].replace("'",'"'))
-        data[-1]["SPO2"]=json.loads(data[-1]["SPO2"].replace("'",'"'))
+        # data[-1]["ECG"]=json.loads(data[-1]["ECG"].replace("'",'"'))
+        # data[-1]["NIBP"]=json.loads(data[-1]["NIBP"].replace("'",'"'))
+        # data[-1]["SPO2"]=json.loads(data[-1]["SPO2"].replace("'",'"'))
         
         if data:
-            Data = {"result":data[-1],"status":"true"}
+            Data = {"result":data,"status":"true"}
             return  Data
         else:
             output = {"result":"No Data Found","status":"false"}
