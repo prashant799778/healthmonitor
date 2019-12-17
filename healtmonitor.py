28f984fbe6d9ce88f5e261aab0f5abbd208cfd22
prashant <prashantgoyal.fourbrick@gmail.com>
2019-12-17 21:02:38 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -539,7 +539,7 @@ def Patient_master_select():
             y = y +  WhereCondition1
 
        
-        query = "select * from Patient_master  " + y + " and  enddate = 'NULLL'"
+        query = "select * from Patient_master  " + y + " and  enddate='NULL' "
         conn=Connection()
         cursor = conn.cursor()
         cursor.execute(query)
