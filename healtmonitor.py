9eff98bd686c30f370c52f76047816844913d47d
prashant <prashantgoyal.fourbrick@gmail.com>
2019-12-17 11:05:55 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -586,12 +586,13 @@ def Patient_Vital_master_select():
         data = cursor.fetchall()
         cursor.close()
         for  data1 in data:
+
             data[data1]["ECG"]=json.loads(data[data1]["ECG"].replace("'",'"'))
             data[data1]["NIBP"]=json.loads(data[data1]["NIBP"].replace("'",'"'))
             data[data1]["SPO2"]=json.loads(data[data1]["SPO2"].replace("'",'"'))
 
         
-        print("prashant",data1)
+        #print("prashant",data1)
         data[-1]["ECG"]=json.loads(data[-1]["ECG"].replace("'",'"'))
         data[-1]["NIBP"]=json.loads(data[-1]["NIBP"].replace("'",'"'))
         data[-1]["SPO2"]=json.loads(data[-1]["SPO2"].replace("'",'"'))
