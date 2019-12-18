dc84f6fd0ec4a76b02bed454d6c78b193aeac072
sharad <sharad.fourbrick@gmail.com>
2019-12-19 00:41:23 +0530

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -2,10 +2,10 @@ import paho.mqtt.client as mqtt
 import json
 
 
-def on_connect(client, userdata, flags, rc):
-  print("-------Connected-------")
-  client.subscribe("outTopic")
-  client.publish("outTopic", "Hello world!");
+# def on_connect(client, userdata, flags, rc):
+  # print("-------Connected-------")
+  # client.subscribe("outTopic")
+  # client.publish("outTopic", "Hello world!");
 
 def on_message(client, userdata, msg):    
   data = msg.payload.decode('utf-8')
@@ -15,7 +15,7 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.connect("159.65.146.25",1883,60)
 
-client.on_connect = on_connect
+#client.on_connect = on_connect
 client.on_message = on_message
 
 client.loop_forever()
