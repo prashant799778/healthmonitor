94cc70de18bc11da8581c2dffb204fd57a45143e
sharad <sharad.fourbrick@gmail.com>
2019-12-19 00:38:07 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 client = mqtt.Client()
 client.connect("159.65.146.25",1883,60)
-client.publish("outTopic", "Hello world!")
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5054, debug=True) 
+while True:
+    client.publish("outTopic", "Hello world!")
+# if __name__ == '__main__':
+    # app.run(host='0.0.0.0', port=5054, debug=True) 
