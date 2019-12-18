2a5aa5fca1e9ff53878f8aba020375182150c179
sharad <sharad.fourbrick@gmail.com>
2019-12-19 01:31:16 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -8,6 +8,6 @@ app.config['SECRET_KEY'] = 'secret!'
 client = mqtt.Client()
 client.connect("159.65.146.25",1883,60)
 while True:
-    client.publish("outTopic", "Hello world!")
+    client.publish("outTopic", "Hello world11111111111111111")
 # if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5054, debug=True) 
