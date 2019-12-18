5571cef4dbb7b62b8245c936b4a47bbfb2645ca3
sharad <sharad.fourbrick@gmail.com>
2019-12-18 11:43:27 +0530

    Update so.py

--- so.py
+++ so.py
@@ -11,19 +11,19 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 
-# @socketio.on('message')
-# def handle_message(message):
-    # print('received message: ' + message)
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
 
 
-@socketio.on('new message')
-def handle_json(json):
-    print('received json: ' + str(json))
-    socketio.emit(json)
-    data=json  
+# @socketio.on('new message')
+# def handle_json(json):
+    # print('received json: ' + str(json))
+    # socketio.emit(json)
+    # data=json  
 
-    socketio.send(data) 
-    socketio.emit(data)
+    # socketio.send(data) 
+    # socketio.emit(data)
     
 # @socketio.on('my event')
 # def handle_my_custom_event(json):
