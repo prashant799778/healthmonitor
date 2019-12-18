906e38bee8ce63165cfa5dbbd9e900b5ce260d90
sharad <sharad.fourbrick@gmail.com>
2019-12-18 20:07:11 +0530

    test

---
+++ consumet.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO,emit,send
+import socketio
+#import flaskext.couchdb
+#from flask.ext.socketio import SocketIO
+from flask_cors import CORS
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+    emit('message', message, broadcast=True)
+    print("ok")
+    emit('newmessage', message, broadcast=True)
+    print("newmessage")
+    #send(message, broadcast=True)
+
+
+# @socketio.on('new message')
+# def handle_json(json):
+    # print('received json: ' + str(json))
+    # socketio.emit(json)
+    # data=json  
+
+    # send(data) 
+    # semit(data)
+    
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))    
+    
+if __name__ == '__main__':
+    CORS(app, support_credentials=True)
+    app.run(host='0.0.0.0', port=5054, debug=True) 
+    socketio.run(app)
