0ad8a41ab76bc1894af8aa205ae00214725ef8df
sharad <sharad.fourbrick@gmail.com>
2019-12-18 16:46:35 +0530

    Update so.py

--- so.py
+++ so.py
@@ -7,30 +7,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
-import logging 
-  
-#Create and configure logger 
 
-  
-#Creating an object 
-logger=logging.getLogger() 
-  
-#Setting the threshold of logger to DEBUG 
-logger.setLevel(logging.DEBUG)
 
 
 
 @socketio.on('message')
 def handle_message(message):
     print('received message: ' + message)
-    logging.basicConfig(filename="/var/www/HealthCare/Healthmonitor/newfile.log", 
-                    format='%(message)s', 
-                    filemode='w') 
-    logger=logging.getLogger() 
-  
-    #Setting the threshold of logger to DEBUG 
-    logger.setLevel(logging.DEBUG)
-    #emit('my response', message, broadcast=True)
+    emit('my response', message, broadcast=True)
 
 @socketio.on('new message')
 def handle_json(json):
