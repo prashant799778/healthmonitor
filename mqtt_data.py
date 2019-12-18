43155b16e5a585f9b3ff29060153fe84e5282eb2
sharad <sharad.fourbrick@gmail.com>
2019-12-18 19:03:52 +0530

    Update mqtt_data.py

--- mqtt_data.py
+++ mqtt_data.py
@@ -1,3 +1,25 @@
+# from flask import Flask, render_template
+# from flask_socketio import SocketIO
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'secret!'
+# socketio = SocketIO(app)
+
+
+
+
+# @socketio.on('message')
+# def handle_message(message):
+    # print('received message: ' + message)
+
+
+# @socketio.on('json')
+# def handle_json(json):
+    # print('received json: ' + str(json))
+
+# @socketio.on('my event')
+# def handle_my_custom_event(json):
+    # print('received json: ' + str(json))    
 import paho.mqtt.client as mqtt
 import os, urlparse
 
@@ -41,5 +63,12 @@ mqttc.subscribe(topic, 0)
 
 # Publish a message
 mqttc.publish(topic, "my message")
-
+rc = 0
+while rc == 0:
+    rc = mqttc.loop()
+print("rc: " + str(rc))
 # Continue the network loop, exit when an error occurs
+   
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5054, debug=True) 
+    # socketio.run(app)
