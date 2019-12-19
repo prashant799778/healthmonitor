959c2dbb05c6724027f0975e4cc83ba0b949983a
sharad <sharad.fourbrick@gmail.com>
2019-12-20 02:50:17 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -186,7 +186,7 @@ def allPatient():
         conn=Connection()
         cursor = conn.cursor()
         cursor.execute(query)
-        data= cursor.fetchone()
+        data= cursor.fetchall()
         cursor.close()
         if data:
             return {"result":data,"status":"true"}
