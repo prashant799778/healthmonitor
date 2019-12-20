141a3b32764a96ef5f3015b55caa99e72a7f52b9
sharad <sharad.fourbrick@gmail.com>
2019-12-20 12:51:54 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -269,6 +269,7 @@ def doctorLoginHospital():
         print(json1)
         data=json.loads(json1.decode("utf-8"))
         query="select ID, HospitalId from DoctorMaster where Email='"+str(data[Email])+"';"
+        print(query)
         conn=Connection()
         cursor = conn.cursor()
         cursor.execute(query)
