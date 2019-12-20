f4f1069db3b16f4b1ac80af5cba7f8ace86a2427
prashant <prashantgoyal.fourbrick@gmail.com>
2019-12-20 17:35:43 +0530

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -12,8 +12,10 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
 	try:
 		data = msg.payload.decode('utf-8')
-		#client.publish("outTopic1","data111111")  
+		#client.publish("outTopic1","data111111") 
+		data= json.loads(data.decode("utf-8"))   
 		print(data)
+
 		query2  = " insert into Patient_Vital_master(Patient_Id,RESP,ECG,SPO2,NIBP,TEMP,usercreate)"
 		query2 =query2 +" values("+'"'+str(data["PatientId"])+'"'+','+'"'+str(data["RESP"])+'"'+','+'"'+str(data["ECG"])+'"'+','+'"'+str(data["SPO2"])+'"'+','+'"'+str(data["NIBP"])+'"'+','+'"'+str(data["TEMP"])+'"'+','+'"'+str(data["usercreate"])+'"'+' '+");"
 		print(query2)
