ea8a28dd386a6a615400b1c5aafb0612e8ec3857
prashant <prashantgoyal.fourbrick@gmail.com>
2019-12-20 17:55:21 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -111,7 +111,7 @@ def login8888():
         cursor = conn.cursor()
         cursor.execute(query)
         loginuser = cursor.fetchone()
-        cursor.close()
+       
 
        
         y= loginuser["Usertype"]
@@ -122,15 +122,18 @@ def login8888():
 
 
         if  y == 'Nurse':
-            query2 = "select  hm.hospital_name  as hospital_Name,Dm.DoctorName as DoctorName,Dm.Email as Doctor_Email,Dm.ID as DoctorID  from Hospital_master  as hm INNER JOIN DoctorMaster as Dm on Dm.HospitalId= hm.ID where  hm.ID ='" + str(y2) + "'"
-            conn=Connection()
+            query2 = "select  hm.hospital_name  as hospital_Name,hm.HubId as HubId,Dm.DoctorName as DoctorName,Dm.Email as Doctor_Email,Dm.ID as DoctorID  from Hospital_master  as hm INNER JOIN DoctorMaster as Dm on Dm.HospitalId= hm.ID where  hm.ID ='" + str(y2) + "'"
+           
             cursor = conn.cursor()
             cursor.execute(query2)
             Nurse = cursor.fetchall()
-            cursor.close()
-
+            l=[]
+            for i in Nurse:
+                doctor= i["DoctorName"]
+                l.append(doctor) 
+            
         query2 = "select  * from Patient_master where Status<>'2'  and Usertype_Id ='" + str(y3) + "'"
-        conn=Connection()
+      
         cursor = conn.cursor()
         cursor.execute(query2)
         ii= cursor.fetchone()
@@ -142,7 +145,7 @@ def login8888():
             Count=0
 
         if loginuser:   
-            data={"status":"true","result":loginuser,"Nurse Details":Nurse,"Patient Details":ii,"Count":Count}                      
+            data={"status":"true","result":loginuser,"Nurse Details":Nurse,"Patient Details":ii,"Count":Count,"DoctorName":l}                      
             return data
         else:
             data={"status":"false","result":"Login Failed"}
