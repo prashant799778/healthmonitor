1096ec74a6b5bd2f50e25373aea56c78d3aa3df3
sharad <sharad.fourbrick@gmail.com>
2019-12-20 17:01:17 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -397,9 +397,9 @@ def doctorLoginDashboard():
         
         cursor.close()
         if data:
-            # data.append({"Total_hospital":len(data)})
-            # data.append({"total_patient":total_patient})
-            return {"result":data,{"Total_hospital":len(data)},{"total_patient":total_patient},"status":"true"}
+            data.append({"Total_hospital":len(data)})
+            data.append({"total_patient":total_patient})
+            return {"result":data,"status":"true"}
         else:
             return {"result":"No Record Found","status":"true"}
     
