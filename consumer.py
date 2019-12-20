5a371833691a9799b171c8974e2b44b41e10031d
prashant <prashantgoyal.fourbrick@gmail.com>
2019-12-20 17:30:48 +0530

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,5 +1,7 @@
 import paho.mqtt.client as mqtt
 import json
+from config import Connection
+
 
 
 def on_connect(client, userdata, flags, rc):
@@ -12,16 +14,14 @@ def on_message(client, userdata, msg):
 		data = msg.payload.decode('utf-8')
 		#client.publish("outTopic1","data111111")  
 		print(data)
-       
-        
-        query2  = " insert into Patient_Vital_master(Patient_Id,RESP,ECG,SPO2,NIBP,TEMP,usercreate)"
-        query2 =query2 +" values("+'"'+str(data["PatientId"])+'"'+','+'"'+str(data["RESP"])+'"'+','+'"'+str(data["ECG"])+'"'+','+'"'+str(data["SPO2"])+'"'+','+'"'+str(data["NIBP"])+'"'+','+'"'+str(data["TEMP"])+'"'+','+'"'+str(data["usercreate"])+'"'+' '+");"
-        print(query2)
-        conn=Connection()
-        cursor = conn.cursor()
-        cursor.execute(query2)
-        conn.commit()
-        cursor.close()
+		query2  = " insert into Patient_Vital_master(Patient_Id,RESP,ECG,SPO2,NIBP,TEMP,usercreate)"
+		query2 =query2 +" values("+'"'+str(data["PatientId"])+'"'+','+'"'+str(data["RESP"])+'"'+','+'"'+str(data["ECG"])+'"'+','+'"'+str(data["SPO2"])+'"'+','+'"'+str(data["NIBP"])+'"'+','+'"'+str(data["TEMP"])+'"'+','+'"'+str(data["usercreate"])+'"'+' '+");"
+		print(query2)
+		conn=Connection()
+		cursor = conn.cursor()
+		cursor.execute(query2)
+		conn.commit()
+		cursor.close()
        
         
     except Exception as e :
