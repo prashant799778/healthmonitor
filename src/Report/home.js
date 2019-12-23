53f9177aac9e91cbdf12b9c4f9e5a6a4a1a84808
sharad <sharad.fourbrick@gmail.com>
2019-12-23 13:57:30 +0530

    test

--- src/Report/home.js
+++ src/Report/home.js
@@ -93,7 +93,7 @@ totalDoctorCount:"",
           <div className="row">
             <div className="col-sm-12 col-md-12">
               <div className="page-hadding">
-                <h2>Dashborad</h2>
+                <h2>Dashboard</h2>
                 <h3>Monitoring </h3>
               </div>
             </div>
