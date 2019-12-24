7559408ece53164f2cacf322768a9fc3ad83a908
sharad <sharad.fourbrick@gmail.com>
2019-12-24 18:24:32 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -273,8 +273,8 @@ def allHospital():
 def allDoctor():
     try:
         
-        query= "select DM.ID,DM.DoctorName,HM.hospital_name,(HM.Address)hospital_address,HM.ID as Hospital_Id,HBS.ID as HubId"
-        query=query+"(select count(*) as count from Patient_master where DM.ID=Patient_master.DoctorID)patient,"
+        query= "select DM.ID,DM.DoctorName,HM.hospital_name,(HM.Address)hospital_address,HM.ID as Hospital_Id,HBS.ID as HubId,"
+        query=query+"(select count(*) as count from Patient_master where DM.ID=Patient_master.DoctorID) patient,"
         query=query+"HBS.HubName from DoctorMaster DM,Hospital_master HM,HubMaster as HBS where  DM.HospitalId=HM.ID and HM.HubId=HBS.ID;"
         conn=Connection()
         cursor = conn.cursor()
