0c78bbddf9658bc984302ce1c6ecad2db9f1280c
prashant <prashantgoyal.fourbrick@gmail.com>
2019-12-24 16:00:15 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -1299,40 +1299,40 @@ def addUser():
 
 
 
+# @app.route('/addDoctor', methods=['POST'])
+# def addDoctor():
+#     try:
+#         json1=request.get_data() 
+#         data1=json.loads(json1.decode("utf-8"))  
+        
+#         query = "select * from DoctorMaster where Email='"+str(data1["Email"])+ "';"
+#         conn=Connection()
+#         cursor = conn.cursor()
+#         cursor.execute(query)
+#         data = cursor.fetchall()
+#         cursor.close()
+#         print(data)
+#         if data==():           
+#             query2  = " insert into DoctorMaster (HospitalId,DoctorName,Email,Gender)"
+#             query2 = query2 +" values('"+str(data1["HospitalId"])+"','"+str(data1["DoctorName"])+"','"+str(data1["Email"])+"','"+str(data1["Gender"])+"');"
+#             print(query2)
+#             conn=Connection()
+#             cursor = conn.cursor()
+#             cursor.execute(query2)
+#             conn.commit()
+#             output = {"result":"data inserted successfully","status":"true"}
+#             return output
+#         else:
+#             output = {"result":"record already Exist","status":"true"}
+#             return output 
+#     except Exception as e :
+#         print("Exception---->" + str(e))    
+#         output = {"result":"something went wrong","status":"false"}
+#         return output
+
+
 @app.route('/addDoctor', methods=['POST'])
 def addDoctor():
-    try:
-        json1=request.get_data() 
-        data1=json.loads(json1.decode("utf-8"))  
-        
-        query = "select * from DoctorMaster where Email='"+str(data1["Email"])+ "';"
-        conn=Connection()
-        cursor = conn.cursor()
-        cursor.execute(query)
-        data = cursor.fetchall()
-        cursor.close()
-        print(data)
-        if data==():           
-            query2  = " insert into DoctorMaster (HospitalId,DoctorName,Email,Gender)"
-            query2 = query2 +" values('"+str(data1["HospitalId"])+"','"+str(data1["DoctorName"])+"','"+str(data1["Email"])+"','"+str(data1["Gender"])+"');"
-            print(query2)
-            conn=Connection()
-            cursor = conn.cursor()
-            cursor.execute(query2)
-            conn.commit()
-            output = {"result":"data inserted successfully","status":"true"}
-            return output
-        else:
-            output = {"result":"record already Exist","status":"true"}
-            return output 
-    except Exception as e :
-        print("Exception---->" + str(e))    
-        output = {"result":"something went wrong","status":"false"}
-        return output
-
-
-@app.route('/addDoctor1', methods=['POST'])
-def addDoctor1():
     try:
         json1=request.get_data() 
         data1=json.loads(json1.decode("utf-8"))  
