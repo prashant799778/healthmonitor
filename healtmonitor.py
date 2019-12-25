13d6dd9fc9c8ffc29759d251e363ce12d5d08bd8
sharad <sharad.fourbrick@gmail.com>
2019-12-25 23:33:14 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -1834,7 +1834,7 @@ def userM():
         # json1=request.get_data() 
         # data=json.loads(json1.decode("utf-8")) 
         print("!1111111111")
-        query1 = " select password from healthmonitor_staging.userMaster where Email='rakesh@gmail.com' ;"
+        query1 = " select Hospital_Id from healthmonitor_staging.userMaster where Email='rakesh@gmail.com' ;"
         print("!22222222")
         print(query1)
         conn=Connection()
@@ -1842,8 +1842,9 @@ def userM():
         cursor.execute(query1)
         data2 = cursor.fetchall()
         cursor.close()
+        data3=data2["Hospital_Id"].replace('"',"'")
         print(data2)
-        query = " select um.ID,um.name,um.Email,um.Gender,hm.hospital_name from healthmonitor_staging.userMaster um,healthmonitor_staging.Hospital_master hm where hm.ID in(1) and um.Email='rakesh@gmail.com';"
+        query = " select um.ID,um.name,um.Email,um.Gender,hm.hospital_name from healthmonitor_staging.userMaster um,healthmonitor_staging.Hospital_master hm where hm.ID in("+ data3 +")+ and um.Email='rakesh@gmail.com';"
         print("!22222222")
         print(query)
         conn=Connection()
