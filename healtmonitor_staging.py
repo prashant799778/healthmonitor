f9ee27b928a232c0c4e5928e79b79cb50a1405d5
sharad <sharad.fourbrick@gmail.com>
2019-12-26 18:57:32 +0530

    Update healtmonitor_staging.py

--- healtmonitor_staging.py
+++ healtmonitor_staging.py
@@ -1321,7 +1321,7 @@ def doctorProfile():
         
         
         cursor.close()
-        output = {"result":data,"hospital":data1[0],"hub_count":hub_count,"status":"true"}
+        output = {"result":data,"hub_count":hub_count,"status":"true"}
         output["hospital_count"]=data1[0]["hospital"]
         return output  
     
