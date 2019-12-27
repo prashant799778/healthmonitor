6c6daf19bd7b55294ba97850de36e2c52433764b
sharad <sharad.fourbrick@gmail.com>
2019-12-27 19:24:47 +0530

    Update healtmonitor_staging.py

--- healtmonitor_staging.py
+++ healtmonitor_staging.py
@@ -337,7 +337,7 @@ def doctorLoginDashboard():
             data2= cursor.fetchall()
             print(data2)
             i["patient_Details"]=data2
-        
+            print("111111111111111111")
             query3="select count(*) as count from Patient_master pm,patientDoctorMapping pdm where pdm.Patient_Id=pm.PatientId" 
             query3=query2+" and pdm.doctorId='"+str(i["doctorId"]) +"' and pm.hospitalId='"+str(i["hospitalId"])+"';"
             cursor.execute(query3)
