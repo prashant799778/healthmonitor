6065397a17f74ddaa6ffcb06dae3de6da0480983
prashant <prashantgoyal.fourbrick@gmail.com>
2019-12-27 11:08:07 +0530

    Update healtmonitor_staging.py

--- healtmonitor_staging.py
+++ healtmonitor_staging.py
@@ -154,7 +154,7 @@ def allHospital():
             data1 = cursor.fetchall()
             
             i["total_doctor"]=data1[0]["count"]
-            query2="select mpum.userId as ID  from userMaster as um ,userHospitalMapping  as mpum,HubMaster as Hbs,Hospital_master as hm  where  mpum.userId=um.ID and mpum.hospitalId=hm.ID and  hm.HubId=Hbs.ID  and  um.Usertype_Id=2  and  hm.ID='"+str(i["ID"])+"';"
+            query2="select um.ID as ID  from userMaster as um ,userHospitalMapping  as mpum,Patient_master as Pm,HubMaster as Hbs,Hospital_master as hm  where  Pm.hospitalId=hm.ID and mpum.userId=um.ID and mpum.hospitalId=hm.ID and  hm.HubId=Hbs.ID  and  um.Usertype_Id=2  and  hm.ID='"+str(i["ID"])+"';"
             print(query2)
             cursor.execute(query2)
             data2 = cursor.fetchall()
