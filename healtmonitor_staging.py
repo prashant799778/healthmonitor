ca1d9793da0381e729f02ecfc963d9cf07c70132
sharad <sharad.fourbrick@gmail.com>
2019-12-28 22:11:00 +0530

    Update healtmonitor_staging.py

--- healtmonitor_staging.py
+++ healtmonitor_staging.py
@@ -466,7 +466,7 @@ def HospitalPatientDetails():
             query3= query3 + " from Patient_master  as PM ,patientDoctorMapping as pdm,Hospital_master as Hm,HubMaster as Hbs " 
             query3=query3+" where PM.hospitalId=Hm.ID and Hm.HubId=Hbs.ID and  pdm.Patient_Id=PM.PatientId  and PM.Status<>'2'   and "
             query3=query3+" pdm.doctorId='" + str(doctor_Id) + "' and PM.hospitalId='" + str(data["Hospital_Id"] + "'  ORDER BY  PatientId DESC;"   
-            print(query3)
+            #print(query3)
             cursor = conn.cursor()
             cursor.execute(query3)
             data27 = cursor.fetchall()
