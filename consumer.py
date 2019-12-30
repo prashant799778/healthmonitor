2021d0ac5b327b0d344ce8fea10eeabcd6c965bd
sharad <sharad.fourbrick@gmail.com>
2019-12-30 13:41:30 +0530

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -7,7 +7,7 @@ import time
 def on_connect(client, userdata, flags, rc):
     print("-------Connected-------")
     client.subscribe("#")
-    client.publish("topic1","data111111")
+    #client.publish("topic1","data111111")
 	
 def on_message(client, userdata, msg):    
   data = msg.payload.decode('utf-8')
@@ -23,7 +23,7 @@ def on_message(client, userdata, msg):
 		data= json.loads(data)
 		print(data)
 		print("1111111111111")
-		client.publish("topic1",str(data))
+		client.publish("#",str(data))
 		print("2222222222222")
 		print(data)
                 
