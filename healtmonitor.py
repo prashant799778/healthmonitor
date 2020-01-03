00be98af51b3e728e72feb547f2fccdbe3f1b3c9
prashant <prashantgoyal.fourbrick@gmail.com>
2020-01-03 15:16:17 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -1727,7 +1727,7 @@ def Patient_master():
         query2=query2+str(data["lowPressure"])+"','"+str(data["temperature"])+"',"
         query2=query2+(int(data["roomNumber"]))+","+(int(data["gender"]))+","
         query2=query2+(int(data["age"]))+",'"+str(data["BloodGroup"])+"','"
-        query2=query2+str(data["DeviceMac"])+"',"+(data["Bed_Number"])+","
+        query2=query2+str(data["DeviceMac"])+"','"+str(data["Bed_Number"])+"',"
         query2=query2+(int(data["Usertype_Id"]))+","+(int(data["hospitalId"]))+",'"
         query2=query2+str(data["startdate"])+"','"+str(data["usercreate"])+"');"
         print(query2)
