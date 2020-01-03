6a5edbbf29f05e5baa5b416974dff02f75d7ee55
prashant <prashantgoyal.fourbrick@gmail.com>
2020-01-03 15:41:35 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -1718,8 +1718,8 @@ def Patient_master():
     try:
          
         json1=request.get_data() 
-        # data1=json.loads(json1.decode("utf-8")) 
-        data=(json1.decode("utf-8"))
+        data=json.loads(json1.decode("utf-8")) 
+        # data=(json1.decode("utf-8"))
         print(data)
 
                
