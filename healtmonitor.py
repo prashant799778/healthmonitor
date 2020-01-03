3145dc06f2166f5d302da0e6bbeaa7821b542b4c
sharad <sharad.fourbrick@gmail.com>
2020-01-03 13:21:21 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -505,7 +505,7 @@ def doctorLoginDashboard():
             cursor.execute(query2)
             data2= cursor.fetchall()
             for j in data2:
-                if j["heartRate"]!=NULL:
+                
                 j["heartRate"]=json.loads(j["heartRate"].replace("'",'"'))
                 print('j["heartRate"]',j["heartRate"])
                 j["highPressure"]=json.loads(j["highPressure"].replace("'",'"'))
@@ -1671,24 +1671,17 @@ def operationDashboard():
         
         json1=request.get_data()
         Data=json.loads(json1.decode("utf-8"))
-        query3 ="select  PM.PatientId as ID,PM.PatientName,PM.PhoneNo,Hbs.HubName,PM.Address,PM.BloodGroup,PM.DeviceMac,Hm.HubId,Hm.hospital_name as hospital_Name, "
+        query3 ="select  PM.PatientId as ID,PM.PatientName,PM.PhoneNo,Hbs.HubName,PM.Address,PM.BloodGroup,PM.DeviceMac,Hm.HubId,Hm.hospital_name as hospital_Name , "
         query3=query3+" PM.Email,PM.Bed_Number,PM.Usertype_Id,PM.age,PM.Gender,PM.roomNumber,pdm.DoctorID as DoctorID"
         query3= query3 + " from Patient_master  as PM ,patientDoctorMapping as pdm,Hospital_master as Hm,HubMaster as Hbs  where PM.hospitalId=Hm.ID  and  Hm.ID='"+str(Data["hospital_Id"])+"' and  Hm.HubId=Hbs.ID and  pdm.Patient_Id=PM.PatientId  and PM.Status<>'2'  Limit    " + str(Data["startlimit"]) + ", " + str(Data["endlimit"]) + " ;"
         conn=Connection()
         cursor = conn.cursor()
         cursor.execute(query3)
         data= cursor.fetchall()
-
-        query ="select  PM.PatientId as ID,PM.PatientName,PM.PhoneNo,Hbs.HubName,PM.Address,PM.BloodGroup,PM.DeviceMac,Hm.HubId,Hm.hospital_name as hospital_Name, "
-        query=query+" PM.Email,PM.Bed_Number,PM.Usertype_Id,PM.age,PM.Gender,PM.roomNumber,pdm.DoctorID as DoctorID"
-        query= query + " from Patient_master  as PM ,patientDoctorMapping as pdm,Hospital_master as Hm,HubMaster as Hbs  where PM.hospitalId=Hm.ID  and  Hm.ID='"+str(Data["hospital_Id"])+"' and  Hm.HubId=Hbs.ID and  pdm.Patient_Id=PM.PatientId  and PM.Status<>'2' ;"
-        
-        cursor.execute(query)
-        data9= cursor.fetchall()
         cursor.close()
 
         if data:
-            return {"result":data,"total_patient":len(data9),"status":"true"}
+            return {"result":data,"status":"true"}
         else:
             return {"result":"No Record Found","status":"true"}
     
