e1d3d2e5aad89aa6978fcd78a9a4e0c0af7da4cc
sharad <sharad.fourbrick@gmail.com>
2020-01-04 13:11:38 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -595,8 +595,8 @@ def hubdoctorLoginDashboard():
         if data1:
             # data.append({"Total_hospital":len(data)})
             # data.append({"total_patient":total_patient})
-            data= {"result":data1,"Total_hub":len(data1),"total_hospital":len(data2),"hospital":data2,"status":"true"}
-            return  data
+            #data= {"result":data1,"Total_hub":len(data1),"total_hospital":len(data2),"hospital":data2,"status":"true"}
+            return  data1
            
         else:
             return {"result":"No Record Found","status":"true"}
