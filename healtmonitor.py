d1f98fc5aa4b3d2bd287e5708156eeedc6cb2d92
prashant <prashantgoyal.fourbrick@gmail.com>
2020-01-23 15:32:23 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -1888,10 +1888,11 @@ def preiscribeMedicine():
         # query = " select distinct userid,username,usertype from usermaster where usertype <> 'Admin';"
         patientId=""
         doctorId=request.args['doctorId']
-        if ' patientId' in request.args:
+        if 'patientId' in request.args:
             patientId=request.args["patientId"]
             print(type(patientId))
             if  (patientId!=0):
+                
                 print("111111")
                 WhereCondition2 =  " and  pmm.patientId    = '" + patientId + "'  "
                 query = "select pmm.id,pmm.patientId,pmm.text,pmm.doctorId,pmm.dateCreate,pm.PatientName from preiscribeMedicine as pmm ,Patient_master as pm where doctorId='" + doctorId + "'and pm.PatientId=pmm.patientId  "+  WhereCondition2 +"  ORDER by pmm.id DESC limit  0,5"
