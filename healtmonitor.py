1a8c92672dfaa44103b0d34a3eb05015ea283092
prashant <prashantgoyal.fourbrick@gmail.com>
2020-01-23 15:15:53 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -1893,8 +1893,9 @@ def preiscribeMedicine():
        
        
         if  patientId!="":
+            print("")
             WhereCondition2 =  " and  pmm.patientId    = '" + patientId + "'  "
-            query = "select pmm.patientId,pmm.text,pmm.doctorId,pmm.dateCreate,pm.PatientName from preiscribeMedicine as pmm ,Patient_master as pm where doctorId='" + doctorId + "'and pm.PatientId=pmm.patientId  "+  WhereCondition2 +"  ORDER by pmm.patientId DESC limit  0,5"
+            query = "select pmm.id,pmm.patientId,pmm.text,pmm.doctorId,pmm.dateCreate,pm.PatientName from preiscribeMedicine as pmm ,Patient_master as pm where doctorId='" + doctorId + "'and pm.PatientId=pmm.patientId  "+  WhereCondition2 +"  ORDER by pmm.id DESC limit  0,5"
             conn=Connection()
             cursor = conn.cursor()
             cursor.execute(query)
@@ -1903,7 +1904,7 @@ def preiscribeMedicine():
         
         else:
             WhereCondition1=""
-            query2 = "select pmm.patientId,pmm.text,pmm.doctorId,pmm.dateCreate,pm.PatientName from preiscribeMedicine as pmm ,Patient_master as pm where doctorId='" + doctorId + "'and pm.PatientId=pmm.patientId  "+  WhereCondition1 +"  ORDER by pmm.patientId DESC limit  0,5"
+            query2 = "select pmm.id,pmm.patientId,pmm.text,pmm.doctorId,pmm.dateCreate,pm.PatientName from preiscribeMedicine as pmm ,Patient_master as pm where doctorId='" + doctorId + "'and pm.PatientId=pmm.patientId  "+  WhereCondition1 +"  ORDER by pmm.id DESC limit  0,5"
             conn=Connection()
             cursor = conn.cursor()
             cursor.execute(query2)
