7d0f27595fe47b7a02482131e6ea2250b7cb26fa
sharad <sharad.fourbrick@gmail.com>
2020-01-23 19:00:07 +0530

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -20,6 +20,7 @@ def on_message(client, userdata, msg):
 		data = msg.payload.decode('utf-8')#client.publish("outTopic1","data111111")
 		t=time.time()
 		print(t*1000)
+		print(data)
 		if "heartRate" not in data:
 			data= json.loads(data)
 		else :
