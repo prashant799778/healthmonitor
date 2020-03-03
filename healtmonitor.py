e124b7702a0ea03dd462ec12f778ae9b71bf851f
vineettomar08 <vineettomar056@gmail.com>
2020-03-04 00:34:55 +0530

    change2

--- healtmonitor.py
+++ healtmonitor.py
@@ -4061,28 +4061,39 @@ def allhospital():
 @app.route('/doctor',methods=['GET'])
 def alldoctor():
 
-    
-    hospital_id = request.args["HospitalId"]
+   try:
+       
+        json1=request.get_data() 
+        data=json.loads(json1.decode("utf-8")) try:
+       
+        json1=request.get_data() 
+        data=json.loads(json1.decode("utf-8")) 
 
-    query = 'select HospitalId from DoctorMaster where HospitalId="'+hospital_id+'"'
-    conn = Connection()
-    cursor = conn.cursor()
-    cursor.execute(query)
-    data1 = cursor.fetchone()
-    print('11111112332434')
-    if data1:
-        query1 = 'select * from DoctorMaster where HospitalId="'+hospital_id+'"'
+        query = 'select HospitalId from DoctorMaster where HospitalId="'+str(data['hospital_id'])+'"'
         conn = Connection()
         cursor = conn.cursor()
-        cursor.execute(query1)
-        data2 = cursor.fetchall()
-        cursor.close()
-        print('637458564')
-        data3={"result":data2}
-        return data3
-    else:
-        output = {"result": "Doctor_data not Found!", "status": "false"}
+        cursor.execute(query)
+        data1 = cursor.fetchone()
+        print('11111112332434')
+        if data1:
+            query1 = 'select * from DoctorMaster where HospitalId="'+hospital_id+'"'
+            conn = Connection()
+            cursor = conn.cursor()
+            cursor.execute(query1)
+            data2 = cursor.fetchall()
+            cursor.close()
+            print('637458564')
+            data3={"result":data2}
+            return data3
+        else:
+            output = {"result": "Doctor_data not Found!", "status": "false"}
+            return output
+    
+    except Exception as e :
+        print("Exception---->" +str(e))    
+        output = {"result":"somthing went wrong","status":"false"}
         return output
+
 
     
 
