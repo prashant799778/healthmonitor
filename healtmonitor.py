8191e886b2dd9eb881b3636e49bea4031349da82
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-06 19:27:07 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -55,11 +55,11 @@ def getLabReportPath(filename):
     return path
 
 
-@app.route("/LabReport/<patientId>/<image_name>")
-def LabReport(patientId,image_name):
+@app.route("/LabReport/<int('"+i"')>/<image_name>")
+def LabReport(i,image_name):
     try:
 
-        return send_from_directory('LabReport/',PatientId=patientId, filename=image_name, as_attachment=False)
+        return send_from_directory('LabReport',PatientId=i, filename=image_name, as_attachment=False)
     except FileNotFoundError:
         abort(404)
 
