bf797cf0f6a5a08eff5c462fe5a9860ca1f01bac
vineettomar08 <vineettomar056@gmail.com>
2020-03-11 13:51:12 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -676,7 +676,7 @@ def allHospital():
         if 'searchFilter' in request.args:
             if request.args['searchFilter'] != "":
                 searchFilter = request.args["searchFilter"]
-                WhereCondition = " where and Hospital_master.hospital_name LIKE '" + "%" + str(searchFilter) + "%" + "'"
+                WhereCondition = " where and HubMaster.HubName LIKE '" + "%" + str(searchFilter) + "%" + "'"
         query="select Hospital_master.ID,Hospital_master.hospital_name,Hospital_master.Address,"
         query=query+"HubMaster.HubName,HubMaster.ID as HubId  from Hospital_master inner join HubMaster on Hospital_master.HubId=HubMaster.ID order by Hospital_master.ID DESC;"
         conn=Connection()
