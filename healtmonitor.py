4606911029eaa8cd295488bc73aa5b92293f362f
vineettomar08 <vineettomar056@gmail.com>
2020-03-11 13:27:45 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -672,9 +672,13 @@ def login1q():
 @app.route('/allHospital', methods=['post'])
 def allHospital():
     try:
-        
+        WhereCondition = ""
+        if 'searchFilter' in request.args:
+            if request.args['searchFilter'] != "":
+                searchFilter = request.args["searchFilter"]
+                WhereCondition = WhereCondition + " and Hospital_master.hospital_name LIKE '" + "%" + str(searchFilter) + "%" + "' OR HubMaster.HubName LIKE '" + "%" + str(searchFilter) + "%" + "'"
         query="select Hospital_master.ID,Hospital_master.hospital_name,Hospital_master.Address,"
-        query=query+"HubMaster.HubName,HubMaster.ID as HubId  from Hospital_master inner join HubMaster on Hospital_master.HubId=HubMaster.ID order by Hospital_master.ID DESC;"
+        query=query+"HubMaster.HubName,HubMaster.ID as HubId  from Hospital_master inner join HubMaster on Hospital_master.HubId=HubMaster.ID where  " + str(WhereCondition) + " order by Hospital_master.ID DESC;"
         conn=Connection()
         cursor = conn.cursor()
         cursor.execute(query)
