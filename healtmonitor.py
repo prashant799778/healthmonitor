fb0f8a92d46fddae32f56a268fc505a6c682dbdf
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-19 17:24:47 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -1020,7 +1020,7 @@ def allPatient1():
         cursor.close()
         if data:
             for i in data:
-                PatientId = i['PatientId']
+                PatientId = i['ID']
                 query1 = "select pdm.doctorId,pdm.Patient_Id as PatientId from patientDoctorMapping as pdm,Patient_Master as pm where pdm.Patient_Id=pm.PatientId and pdm.Patient_Id='" + \
                     str(PatientId)+"'"
                 cursor = conn.cursor()
