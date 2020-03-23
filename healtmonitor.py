8a908ed90144db007f80656485de95306d94a38e
vineettomar08 <vineettomar056@gmail.com>
2020-03-23 18:37:25 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -3062,8 +3062,7 @@ def Patient_masterTest():
         Height = data['Height']
         Weight = data['weight']
         familyHistory = data['familyHistory']
-        # bmi = weight/(Height ** 2)
-        bmi = (Weight / Height / Height) x 10,000
+        bmi = weight/(Height ** 2)
         LandLineNo = data['LandLineNo']
         BuisnessNo = data['BuisnessNo']
         MobileNo1 = data['MobileNo1']
@@ -5474,7 +5473,7 @@ def Patient_masterTest1():
         Height = data['Height']
         Weight = data['weight']
         familyHistory = data['familyHistory']
-        bmi = (Weight / Height / Height) x 10,000
+        bmi = weight/(Height ** 2)
         LandLineNo = data['LandLineNo']
         BuisnessNo = data['BuisnessNo']
         MobileNo1 = data['MobileNo1']
