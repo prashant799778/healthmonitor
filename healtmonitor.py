da8101330fdf2e72b5cad13c5236dda32672e3ab
sharad <sharad.fourbrick@gmail.com>
2020-06-26 17:46:31 +0530

    Update healtmonitor.py

--- healtmonitor.py
+++ healtmonitor.py
@@ -3992,10 +3992,12 @@ def downloadPatientDetails():
        
         json1=request.get_data() 
         data=json.loads(json1.decode("utf-8")) 
-        query1= " select  p.DateCreate,p.Patient_Id,pm.PatientName,p.temperature,p.lowPressure,"
-        query1=query1+"p.highPressure,p.heartRate,p.pulseRate,p.spo2 from Patient_Vital_master p,"
-        query1=query1+"Patient_master pm where pm.PatientId=p.Patient_Id and p.Patient_Id="+str(data["Patient_Id"])+" limit 10;"
-        
+        toDate=datetime.strptime(data["toDate"], '%Y-%m-%d %H:%M:%S') 
+        fromDate=datetime.strptime(data["fromDate"], '%Y-%m-%d %H:%M:%S') 
+        query1= 'select  p.DateCreate,p.Patient_Id,pm.PatientName,p.temperature,p.lowPressure,'
+        query1=query1+'p.highPressure,p.heartRate,p.pulseRate,p.spo2 from Patient_Vital_master p,'
+        query1=query1+'Patient_master pm where pm.PatientId=p.Patient_Id and p.Patient_Id='+str(data["Patient_Id"])
+        query1=query1+' and p.DateCreate <="' +str(toDate) + '" and p.DateCreate >="'+str(fromDate)+ '";'
         print(query1)
         conn=Connection()
         cursor = conn.cursor()
